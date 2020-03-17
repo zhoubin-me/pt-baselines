79193b34213938582f6af0c171adce900ccd0667
Zhou Bin <zmonoid@gmail.com>
2020-03-17 20:01:49 +0800

    fix a3c

--- src/test/test_a3c.py
+++ src/test/test_a3c.py
@@ -12,27 +12,30 @@ class TestAsyncReplay(unittest.TestCase):
 
     def test_async(self):
 
-        env = make_a3c_env('Breakout', 'log/a3c-test')
+        env = make_a3c_env('Pong', 'log/a3c-test')
         mkdir('log/a3c-test')
         s = env.reset()
         network = ACNet(s.shape[0], env.action_space.n)
         network.cuda()
 
         hx, cx = torch.zeros(1, 256).cuda(), torch.zeros(1, 256).cuda()
-        rs = 0
+        step = 0
         while True:
             s = torch.from_numpy(s).unsqueeze(0).cuda()
             v, pi, (hx, cx) = network((s, (hx, cx)))
-
             d = Categorical(logits=pi)
             a = d.sample()
+            if step % 100 == 0:
+                print("{0:6.2f}, {1}, {2:6.2f}, {3:6.2f}, {4}".format(v.item(), 'xx', hx.mean().item(), cx.mean().item(), a.item()))
+
 
             s_next, reward, done, info = env.step(a.item())
-            rs += reward
+            if 'episode' in info:
+                rs = info['episode']['r']
+                print(f"=================={rs}================={step}")
+
             s = s_next
+            step += 1
             if done:
                 s = env.reset()
-                print(rs)
-                print(v.shape, pi.shape, hx.shape, cx.shape)
-                rs = 0
 
