122435b11d6ce57c2e3463566be877fbfeee0d19
zmonoid <zmonoid@gmail.com>
2020-03-24 11:45:37 +0800

    fix

---
+++ src/config.py
@@ -0,0 +1,130 @@
+
+
+class RainbowConfig:
+    game = 'Breakout'
+    seed = 0
+
+    dueling = True
+    double = True
+    prioritize = True
+    noisy = True
+
+    nstep = 3
+    noise_std = 0.5
+    num_atoms = 51
+    v_max = 10
+    v_min = -10
+    history_length = 4
+
+    sgd_update_frequency = 4
+    discount = 0.99
+    batch_size = 32
+    adam_lr = 0.0000625
+    adam_eps = 0.00015
+
+    max_steps = int(5e7)
+    max_episode_steps = 108000
+    log_interval = 10000
+    eval_interval = 100000
+    save_interval = 1000000
+    eval_episodes = 10
+
+    exploration_steps = 20000
+    target_network_update_freq = 8000
+    epsilon_steps = int(1e6)
+    min_epsilon = 0.01
+    test_epsilon = 0.01
+
+    replay_size = int(1e6)
+    replay_alpha = 0.5
+    replay_beta0 = 0.4
+
+    ckpt = ""
+    log_dir = ""
+    play = False
+
+
+class A3CConfig:
+    game = 'Pong'
+    seed = 0
+
+    num_actors = 16
+
+    nsteps = 20
+    discount = 0.99
+    adam_lr = 0.0001
+
+    gae_coef = 1.0
+    entropy_coef = 0.01
+    value_loss_coef = 0.5
+    max_grad_norm = 40
+
+    max_episode_steps = 108000
+    max_steps = int(4e7)
+    eval_episodes = 10
+
+    ckpt = ""
+    log_dir = ""
+    play = False
+
+
+class A2CConfig:
+    game = "Pong"
+    seed = 1234
+
+    num_processes = 16
+    recurrent = True
+
+    nsteps = 5
+    gamma = 0.99
+    lr = 7e-4
+    eps = 1e-5
+    alpha = 0.99
+
+    gae_lambda = 0.95
+    entropy_coef = 0.01
+    value_loss_coef = 0.5
+    max_grad_norm = 0.5
+
+    max_episode_steps = 108000
+    max_steps = int(5e7)
+    log_interval = 8000
+    eval_episodes = 10
+    use_lr_decay = False
+
+    ckpt = ""
+    log_dir = ""
+    play = False
+
+
+class PPOConfig:
+    game = "Pong"
+    seed = 1234
+
+    num_processes = 8
+    recurrent = True
+
+    nsteps = 128
+    gamma = 0.99
+    lr = 2.5e-4
+    eps = 1e-5
+    alpha = 0.99
+    epoches = 4
+    num_mini_batch = 4
+
+
+    gae_lambda = 0.95
+    entropy_coef = 0.01
+    value_loss_coef = 0.5
+    max_grad_norm = 0.5
+    clip_param = 0.1
+
+    max_episode_steps = 108000
+    max_steps = int(5e7)
+    log_interval = nsteps * num_processes * 10
+    eval_episodes = 10
+    use_lr_decay = True
+
+    ckpt = ""
+    log_dir = ""
+    play = False
--- src/run.py
+++ src/run.py
@@ -1,41 +1,19 @@
-import argparse
 import numpy as np
-
+import argparse
 from src.common.utils import set_thread, random_seed, mkdir
 from src.a2c.agent import A2CAgent
+from src.a3c.agent import A3CAgent
+from src.ppo.agent import PPOAgent
+from src.deepq.agent import RainbowAgent
+from .config import RainbowConfig, A2CConfig, A3CConfig, PPOConfig
 
-
-class Config:
-    game = "Pong"
-    seed = 1234
-
-    num_processes = 16
-    recurrent = True
-
-    nsteps = 5
-    gamma = 0.99
-    lr = 7e-4
-    eps = 1e-5
-    alpha = 0.99
-
-    gae_lambda = 0.95
-    entropy_coef = 0.01
-    value_loss_coef = 0.5
-    max_grad_norm = 0.5
-
-    max_episode_steps = 108000
-    max_steps = int(5e7)
-    log_interval = 8000
-    eval_episodes = 10
-    use_lr_decay = False
-
-    ckpt = ""
-    log_dir = ""
-    play = False
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='A2C Hyperparameters')
-    for k, v in Config.__dict__.items():
+    parser = argparse.ArgumentParser(description='Hyperparameters Settings')
+    parser.add_argument('algo', type=str, choices=['Rainbow', 'A2C', 'A3C', 'PPO'])
+    args = parser.parse_known_args()
+    cfg = eval(f'{args[0].algo}Config')
+    for k, v in cfg.__dict__.items():
         if not k.startswith('_'):
             parser.add_argument(f'--{k}', type=type(v), default=v)
     args = parser.parse_args()
@@ -50,10 +28,9 @@ if __name__ == '__main__':
 
     if args.seed is not None:
         random_seed(args.seed)
-    # set_thread(1)
 
     mkdir(args.log_dir)
-    agent = A2CAgent(cfg=args)
+    agent = eval(f'{args.algo}Agent(cfg=args)')
     agent.logger.save_config(args)
 
     if not args.play:
@@ -65,4 +42,3 @@ if __name__ == '__main__':
         returns = agent.eval_episodes()
         print(f"Returns: {returns}")
         print(f"Max: {np.max(returns)}\t Min: {np.min(returns)}\t Std: {np.std(returns)}")
-
