cb75e369912e33fdd07263a57d832cd73e51b7d1
Zhou Bin <zmonoid@gmail.com>
2020-03-25 01:45:22 +0800

    fix a2c

--- delegans/agents/ppo_agent.py
+++ delegans/agents/ppo_agent.py
@@ -68,7 +68,7 @@ class PPOAgent(BaseAgent):
                         self.logger.store(TrainEpRet=info['episode']['r'])
 
             # Compute R and GAE
-            v_next, _ = self.network.get_value(self.rollouts.obs[-1])
+            v_next = self.network.get_value(self.rollouts.obs[-1])
 
             if cfg.use_gae:
                 gae = 0
