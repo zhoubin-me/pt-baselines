7482a10d1d4992024b4d8329ee7e6015906aefdf
Zhou Bin <zmonoid@gmail.com>
2020-04-04 00:51:23 +0800

    trpo atari

--- batch_job.py
+++ batch_job.py
@@ -6,6 +6,7 @@ from src.common.bench import _atari7, _mujoco8
 
 N = torch.cuda.device_count()
 cfgs = glob.glob('src/configs/*.py')
+_mujoco8.pop(-2)
 
 for cfg in cfgs:
     if 'trpo' in cfg and 'mujoco' in cfg:
@@ -18,6 +19,6 @@ for cfg in cfgs:
             os.system(f'export CUDA_VISIBLE_DEVICES="{i % N}";python -m run {cfg} --game {game} --seed 1')
     elif 'mujoco' in cfg:
         for i, game in enumerate(_mujoco8):
-            if game == 'Swimmer':
+            if not 'Inverted' in game:
                 continue
             os.system(f'export CUDA_VISIBLE_DEVICES="{i % N}";python -m run {cfg} --game {game} --seed 1')
