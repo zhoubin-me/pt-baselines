4a84823c4d79628c9c2ebf99b8b555420043d42e
Zhou Bin <zhoubinxyz@gmail.com>
2020-04-08 02:56:28 +0800

    .

--- batch_job.py
+++ batch_job.py
@@ -45,7 +45,7 @@ for cfg in cfgs:
     exps = [
         'ppo_atari', 'trpo_atari',
         'ddpg_mujoco', 'td3_mujoco',
-        'rainbow_atari'
+        'rainbow_atari', 'sac_mujoco'
     ]
 
     if not any(map(lambda x: x in cfg, exps)):
@@ -58,5 +58,4 @@ for cfg in cfgs:
 
     for i, game in enumerate(games):
         hold_on = " " if game == games[-1] else "&"
-        device_id = -1 if 'mujoco' in cfg else i
         os.system(f'python run.py {cfg} --game {game} --seed {seed} --device_id {i} {hold_on}')
